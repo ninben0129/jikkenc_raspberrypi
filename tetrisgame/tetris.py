e0d1be735447643a2ac60d416516e566226a4347
ninben0129 <tanaka.masahiro.r6@dc.tohoku.ac.jp>
2022-12-26 14:23:07 +0900

    コメントの追加

--- tetrisgame/tetris.py
+++ tetrisgame/tetris.py
@@ -10,6 +10,7 @@ FIELD_WIDTH = 5
 
 class Tetris(object):
     def __init__(self, FIELD_HEIGHT, FIELD_WIDTH):
+        "ゲームの初期化"
         self.FIELD = np.zeros((FIELD_HEIGHT+3, FIELD_WIDTH+6))
         self.FIELD_BASE = np.zeros((FIELD_HEIGHT+3, FIELD_WIDTH+6))
         self.FIELD_BASE[:, 0:3] = 1
@@ -21,16 +22,19 @@ class Tetris(object):
         self.nowmino = np.zeros((3, 3))
 
     def init_mino(self):
+        "ミノの一覧の作成"
         self.minolist = np.zeros((1, 3, 3))
         # 縦2ミノ
         self.minolist[0, 0:2, 1] = 1
 
     def gen_mino(self):
+        "ミノの生成"
         self.nowmino = self.minolist[0]
         self.pt = [0, 5]
         self.update()
 
     def keymove(self):
+        "キーによる横移動"
         while True:
             if kb.read_key() == "left":
                 self.nextpt[0] = self.pt[0]
@@ -48,6 +52,7 @@ class Tetris(object):
                     time.sleep(0.2)
 
     def detectCollision(self):
+        "平行移動の衝突検出"
         for i in range(3):
             for j in range(3):
                 if self.FIELD_BASE[self.nextpt[0]+i][self.nextpt[1]-1+j] and self.nowmino[i][j] == 1:
@@ -55,6 +60,7 @@ class Tetris(object):
         return True
 
     def compDetect(self):
+        "行消去の検出，実行"
         sum = np.sum(self.FIELD_BASE, axis=1)
         for i in range(7):
             if sum[i] == 11:
@@ -65,9 +71,11 @@ class Tetris(object):
                 self.FIELD_BASE[0, 3:8] = 0
 
     def gameoverDetect(self):
+        "ゲームオーバーの検出"
         pass
 
     def fallmove(self):
+        "ミノ落下"
         self.nextpt[0] = self.pt[0]+1
         self.nextpt[1] = self.pt[1]
         if self.detectCollision():
@@ -77,11 +85,13 @@ class Tetris(object):
             self.tet_stab()
 
     def tet_stab(self):
+        "地面に触れたミノ固定"
         self.FIELD_BASE = np.copy(self.FIELD)
         self.compDetect()
         self.gen_mino()
 
     def update(self):
+        "表示するFIELDの更新"
         self.FIELD = np.copy(self.FIELD_BASE)
         for i in range(3):
             for j in range(3):
@@ -89,6 +99,7 @@ class Tetris(object):
                                          j] = np.copy(self.FIELD_BASE[self.pt[0]+i][self.pt[1]-1+j]+self.nowmino[i][j])
 
     def game(self):
+        "ゲーム実行部"
         self.init_mino()
         self.gen_mino()
         thread1 = threading.Thread(target=self.keymove)
@@ -101,6 +112,7 @@ class Tetris(object):
             time.sleep(1)
 
     def display(self):
+        "FIELDの表示"
         while True:
             print(self.FIELD)
             time.sleep(0.5)
